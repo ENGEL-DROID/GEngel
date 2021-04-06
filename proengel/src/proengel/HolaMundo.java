50dff4ca7e392c159f3e8bd7e904a7fecb9d46c2
Engel Restrepo <restrepoengelberth@gmail.com>
2021-04-06 22:14:24 +0200

    limpiar segundo

--- proengel/src/proengel/HolaMundo.java
+++ proengel/src/proengel/HolaMundo.java
@@ -2,20 +2,6 @@ package proengel;
 
 public class HolaMundo {
 	
-<<<<<<< HEAD
-	int dividir (int a, int b) {
-=======
-	int multiplicar (int a, int b) {
->>>>>>> branch 'master' of https://github.com/ENGEL-DROID/GEngel.git
-		
-<<<<<<< HEAD
-		return a / b;
-=======
-		return a * b;
->>>>>>> branch 'master' of https://github.com/ENGEL-DROID/GEngel.git
-		
-	}
-	
 	public static void main(String[] args) {
 		
 
