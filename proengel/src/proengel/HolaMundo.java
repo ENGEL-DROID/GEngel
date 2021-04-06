9e93852f2d12964747c1972548e859ec651828d5
Engel Restrepo <restrepoengelberth@gmail.com>
2021-04-06 16:29:07 +0200

    Limpieza método prime rusuario

--- proengel/src/proengel/HolaMundo.java
+++ proengel/src/proengel/HolaMundo.java
@@ -2,19 +2,6 @@ package proengel;
 
 public class HolaMundo {
 	
-<<<<<<< HEAD
-	int restar (int a, int b) {
-		
-		return a - b;
-=======
-	
-	int sumar (int a, int b) {
-		
-		return a + b;
->>>>>>> branch 'master' of https://github.com/ENGEL-DROID/GEngel.git
-		
-	}
-	
 	public static void main(String[] args) {
 		
 
